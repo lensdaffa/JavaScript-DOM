b3043bd335630f134df1bb8a51080338692f6bd3
Aydin Akmal Daffa <lensdaffa@gmail.com>
2026-01-14 20:20:37 +0700

    Add files via upload

---
+++ DOM Manipulation #1/script5.js
@@ -0,0 +1,14 @@
+const judul = document.getElementById('judul')
+judul.innerHTML = '<em>DOM Manipulation 1</em>'
+
+// const sectionA = document.querySelector('#a')
+// sectionA.innerHTML = 'helloooo'
+
+//const judul = document.querySelector('#judul')
+//judul.style.color = 'blue'
+//judul.style.backgroundColor = 'yellow'
+
+// const judul = document.getElementsByTagName('h1')[0]
+// const a = document.querySelector('#a')
+
+// const p2 = document.querySelector('.p2')
